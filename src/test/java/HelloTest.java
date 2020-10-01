61b6d3df6538fe8e7a98098135c677b5d5111648
ZaferBek <zaferbek22@gmail.com>
2020-10-02 00:59:01 +0100

    changes on HelloTest.java file

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,8 @@ public class HelloTest {
     public static void main(String[] args) {
         //some code here
         System.out.println("Hello Test");
+
+        //we are adding some additional lines
+
     }
 }
